8487ba6c685d4f31a71e125c7da9b25d8b53ae5f
Watson <bbcallyou2@gmail.com>
2024-11-25 12:01:07 +0800

    feat[11] play button

--- JavaScript30-master/11 - Custom Video Player/scripts.js
+++ JavaScript30-master/11 - Custom Video Player/scripts.js
@@ -6,9 +6,20 @@ const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
 
+//此處設定目標物播放問題
 function togglePlay() {
     const method = video.paused ? "play" : "pause";
     video[method]();
 }
 
+function updateButton() {
+    const icon = this.paused ? "▶️" : "⏸️";
+}
+
+//這邊做得事，點擊不論影片或是按鈕都會暫停影片
 video.addEventListener("click", togglePlay);
+//這個是建立土泡方法，當播放，影片按件圖案會改變 updateButton func
+video.addEventListener("play", togglePlay);
+video.addEventListener("pause", togglePlay);
+
+toggle.addEventListener("click", togglePlay);
