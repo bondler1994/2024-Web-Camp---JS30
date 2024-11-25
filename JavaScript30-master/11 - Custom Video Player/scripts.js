6eaaf620d3f7fc0e838405190b3ef02c9b3097e7
Watson <bbcallyou2@gmail.com>
2024-11-25 15:39:44 +0800

    feat[11] done

--- JavaScript30-master/11 - Custom Video Player/scripts.js
+++ JavaScript30-master/11 - Custom Video Player/scripts.js
@@ -3,6 +3,7 @@ const video = player.querySelector(".viewer");
 const progress = player.querySelector(".progress");
 const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
+//此處選取任何有data-skip得button
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
 
@@ -18,13 +19,31 @@ function updateButton() {
     toggle.textContent = icon;
 }
 
+// 此處是作skip 功能<檢查html DOM 裡面data-*
+function skip() {
+    console.log(this.dataset.skip);
+    video.currenTime += parseFloat(this.dataset.skip);
+}
 
+//此處要處理音量以及影片播放速度
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+//此處最後 調整影片播放條
+function handleProgress() {
+    const percent = (video.currenTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
 
 //這邊做得事，點擊不論影片或是按鈕都會暫停影片
 video.addEventListener("click", togglePlay);
 //這個是建立土泡方法，當播放，影片按件圖案會改變 updateButton func
-video.addEventListener("play", togglePlay);
-video.addEventListener("pause", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
 
 toggle.addEventListener("click", togglePlay);
-skipButtons;
+skipButtons.forEach((button) => button.addEventListener("click", skip));
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) => range.addEventListener("mousemove", handleRangeUpdate));
